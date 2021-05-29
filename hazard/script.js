5804a36da8f26779bba34c411868bb0d177618bc
Majklcan <majkl.can@seznam.cz>
2021-05-29 22:19:40 +0200

    hazard!

---
+++ hazard/script.js
@@ -0,0 +1,21 @@
+function spin(){
+
+
+	var x = 1024; //min value
+	var y = 9999; // max value
+
+	var deg2 = Math.floor(Math.random() * (x - y)) + y;
+  var deg =3090;
+
+	document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+
+	var element = document.getElementById('mainbox');
+	element.classList.remove('animate');
+	setTimeout(function(){
+		element.classList.add('animate');
+      var element2 = document.getElementsByClassName('alert-result')[0];
+	element2.classList.remove('d-none');
+	}, 5000); 
+  
+
+}
